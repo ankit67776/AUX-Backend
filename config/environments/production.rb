b96ba3de2d053197bb847c5e1cdbdafdda30e853
ankit67776 <ankitraikwal0207#gmail.com>
2025-06-04 08:46:27 +0000

    DB changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,6 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV["RENDER"].present?
+
+  Rails.application.routes.default_url_options[:host] = "https://aux-backend.onrender.com"
 end
