b156beed11d1c024334ea047e08352c34912c9b6
ankit67776 <ankitraikwal0207#gmail.com>
2025-06-04 23:45:05 +0000

    changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,14 @@
+#!/usr/bin/env bash
 set -o errexit
 
 bundle install
+
+# Install any missing migrations (including SolidQueue's)
+bundle exec rails solid_queue:install:migrations
+
+# Precompile assets (optional if you're not using Rails frontend)
 # bin/rails assets:precompile
 # bin/rails assets:clean
 
-# bundle exec rails solid_queue:install:migrations
-# bundle exec rails db:migrate
-
-bin/rails db:migrate
+# Migrate DB after installing migrations
+bundle exec rails db:migrate
