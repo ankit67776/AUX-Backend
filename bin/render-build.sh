b05e01c7b1e61ed11a48912e835aa2256fa0b5c9
ankit67776 <ankitraikwal0207#gmail.com>
2025-06-06 00:16:19 +0000

    changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,9 @@ set -o errexit
 
 bundle install
 
-RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production bundle exec rails db:create
-RAILS_ENV=production bundle exec rails db:migrate
-export RAILS_ENV=production
-
+ #bundle exec rake assets:precompile
+ bundle exec rails db:create
+ bundle exec rails db:migrate
 
 
 bundle exec rails db:create
