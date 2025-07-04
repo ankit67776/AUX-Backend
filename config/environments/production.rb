da73525dac654f316a76906d780fe775d71642ee
NilawarOmkar <omkarnilawar04@gmail.com>
2025-07-04 14:26:21 +0530

    production mode corrected

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            "omkarnilawar04@gmail.com",
-    password:             "ijza lkgb lflm nubl",
+    user_name:            "email",
+    password:             "password",
     authentication:       "plain",
     enable_starttls_auto: true
   }
